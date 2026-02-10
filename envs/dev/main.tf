739dfe4ef204e39bf1c32c46373c22e7db6490a1
ozkioz <kundaoskar@gmail.com>
2026-02-10 17:07:32 +0100

    Add remote backend (S3 + DynamoDB)

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-oskar-123"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 module "ec2" {
   source        = "../../modules/ec2"
   aws_region    = "us-east-1"
